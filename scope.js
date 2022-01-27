745a3a1cf090a0f01e2307a789589f5e93099966
meshkatkanok <mdkanok121@gmail.com>
2022-01-27 18:24:05 +0300

    scope.js

---
+++ scope.js
@@ -0,0 +1,17 @@
+const bonus = 30;
+
+function sum (num1,num2){
+    let result = num1 + num2 + bonus;
+    if (result>10){
+        let frist = "Rifat";
+        let mood= "khairul";
+        console.log(mood);
+    
+    }
+    let swime = "Night";
+    console.log(swime);
+ 
+    return result;
+}
+const output = sum(12, 12);
+console.log(output);
